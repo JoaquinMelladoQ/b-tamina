da065666c4de6a6b52d22cca296e9ed893f45dfe
Rubén Ignacio Castillo Díaz <ruben.cas@outlook.com>
2021-01-07 13:02:47 -0300

    Home#index and Brewery registration form ready

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'home#index'
   devise_for :clients
-  devise_for :breweries
+  devise_for :breweries, controllers: { registrations: 'breweries/registrations' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 end
