93259ccc303d5ef2aa51a448ee8af50a39ef1076
Rubén Ignacio Castillo Díaz <ruben.cas@outlook.com>
2021-01-07 12:41:55 -0300

    brewery registration form created, sanitizer missing

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  root 'home#index'
   devise_for :clients
   devise_for :breweries
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
 end
