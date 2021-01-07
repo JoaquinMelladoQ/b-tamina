3ba91d9e94944a4f4d9bc1950e9351618cbd611e
Jose Cordero Fuentealba <jocorderf@gmail.com>
2021-01-07 18:03:30 -0300

    Defined create method in deals controller

--- app/controllers/deals_controller.rb
+++ app/controllers/deals_controller.rb
@@ -2,4 +2,22 @@ class DealsController < ApplicationController
   def new
     @deal = Deal.new
   end
+
+  def create
+    bagasse = Bagasse.where('brewery_id = ', params[:brewery_id]).pluck[:quantity]
+    @total = bagasse.inject(0) { |sum, b| sum + b }
+    if params[:quantity] < @total 
+      @deal = Deal.create(deal_params)
+      @deal.save!
+      redirect_to 'root_path'
+    else 
+      redirect_to 'new_deal_path'
+    end
+  end 
+
+  protected
+
+  def deal_params
+    params.require(:deal).permit(:deal_quantity, :deal_date, :brewery_id)
+  end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,7 @@ Rails.application.routes.draw do
   devise_for :clients, controllers: { registrations: 'clients/registrations'}
   get 'breweries/index', to: 'breweries#index', as: 'brewerie_index'
   get 'clients/index', to: 'clients#index', as: "clients_index"
-  resources :deals
-  
+  resources :deals 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 end
