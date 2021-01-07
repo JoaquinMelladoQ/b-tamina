c47fa4f1ef57af910b48dadee65b48616226ccec
Jose Cordero Fuentealba <jocorderf@gmail.com>
2021-01-07 10:33:45 -0300

    Modified Brewery and Client model to specify N to N relationship

--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -1,4 +1,6 @@
 class Brewery < ApplicationRecord
+  has_many :deals 
+  has_many :clients, through: :deals
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
--- app/models/client.rb
+++ app/models/client.rb
@@ -1,4 +1,6 @@
 class Client < ApplicationRecord
+  has_many :deals 
+  has_many :breweries, through: :deals
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
