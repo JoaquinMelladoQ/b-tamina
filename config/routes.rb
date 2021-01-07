c4ee36b140031a03cb0c31e6a93f989fbf9e0f96
Jose Cordero Fuentealba <jocorderf@gmail.com>
2021-01-07 16:05:42 -0300

    Added get route to deals new view in route.rb

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   devise_for :clients, controllers: { registrations: 'clients/registrations'}
   get 'breweries/index', to: 'breweries#index', as: 'brewerie_index'
   get 'clients/index', to: 'clients#index', as: "clients_index"
+  get 'deals/new', to:'deals#new', as: 'new_deal'
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
