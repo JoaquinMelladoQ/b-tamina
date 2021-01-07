64855246473df879f24379f52a88949fc2f387ea
Rubén Ignacio Castillo Díaz <ruben.cas@outlook.com>
2021-01-07 16:16:09 -0300

    Bagasse creation (routes,view, controller) ready

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   
+  resources :bagasses, only: [:new, :create]
   root 'home#index'
   devise_for :breweries, controllers: { registrations: 'breweries/registrations' }
   devise_for :clients, controllers: { registrations: 'clients/registrations'}
