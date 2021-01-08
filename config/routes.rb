a3a1255e99000c16fcc4dee6b5c2b749e90da1f1
Jose Cordero Fuentealba <jocorderf@gmail.com>
2021-01-08 03:29:09 -0300

    Added show route for bagasse in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   
-  resources :bagasses, only: [:new, :create]
+  resources :bagasses, only: [:new, :create, :show]
   devise_for :breweries, controllers: { registrations: 'breweries/registrations' }
   devise_for :clients, controllers: { registrations: 'clients/registrations'}
   get 'breweries/index', to: 'breweries#index', as: 'brewerie_index'
