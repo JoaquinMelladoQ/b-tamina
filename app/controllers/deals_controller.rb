e026f465085439debefd842287ef7b1fa24dbb86
Rubén Ignacio Castillo Díaz <ruben.cas@outlook.com>
2021-01-07 22:16:48 -0300

    brewery id by params

--- app/controllers/deals_controller.rb
+++ app/controllers/deals_controller.rb
@@ -1,10 +1,11 @@
 class DealsController < ApplicationController
   def new
     @deal = Deal.new
+    @brewery=Brewery.find(params[:brewery_id])
   end
 
   def create
-    bagasse = Bagasse.where('brewery_id = ', params[:brewery_id]).pluck[:quantity]
+    bagasse = Bagasse.where(brewery_id: params[:brewery_id]).pluck[:quantity]
     @total = bagasse.inject(0) { |sum, b| sum + b }
     if params[:quantity] < @total 
       @deal = Deal.create(deal_params)
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,9 @@ Rails.application.routes.draw do
   get 'breweries/index', to: 'breweries#index', as: 'brewerie_index'
   get 'clients/index', to: 'clients#index', as: "clients_index"
   get 'deals/:brewery_id', to: 'deals#new', as:'new_deal_brewery_id'
+  post 'deals/create/:brewery_id', to: 'deals#create', as:'create_deal_brewery_id' 
   root 'home#index'
+
   
   resources :deals 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
