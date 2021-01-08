257645b7c4f43fa62bc06cb1b8c5aac28ccda422
Rubén Ignacio Castillo Díaz <ruben.cas@outlook.com>
2021-01-08 03:04:37 -0300

    3AM

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -4,7 +4,7 @@ class ClientsController < ApplicationController
   end
 
   def show 
-    @client = Client.find(id: params[:id])
+    @client = Client.find(params[:id])
     @deals = Deal.where(client_id: @client.id, brewery_id: current_brewery.id)
   end
 end
