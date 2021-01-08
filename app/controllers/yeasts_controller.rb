2d51e369344133abf731c3133d721c7ea146f613
Jose Cordero Fuentealba <jocorderf@gmail.com>
2021-01-08 08:46:20 -0300

    Generated yeasts controller

---
+++ app/controllers/yeasts_controller.rb
@@ -0,0 +1,25 @@
+class YeastsController < ApplicationController
+    def new
+      @yeast=Yeast.new
+    end
+  
+    def show
+      @yeasts = Yeast.where(bewery_id: current_brewery.id)
+    end
+  
+    def create
+      @yeast=Yeast.new(quantity: yeast_params[:quantity], production_date: yeast_params[:production_date], brewery_id: current_brewery.id)
+      if @bagasse.save
+          redirect_to root_path, notice: 'Register was successfully created.'
+      else
+          flash.now[:alert] = 'Register cannot be created.'
+          render :index
+      end
+    end
+  
+    private
+      def bagasse_params
+        params.require(:yeast).permit(:quantity, :production_date)
+      end
+  
+  end
